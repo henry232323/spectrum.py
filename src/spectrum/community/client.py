f2b04e3c6c940b0b0282f1378d4be94f0f6e273c
henry232323 <14969260+henry232323@users.noreply.github.com>
2023-12-11 23:12:17 +0900

    Fix lookup

--- src/spectrum/community/client.py
+++ src/spectrum/community/client.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from aiographql.client import GraphQLClient, GraphQLRequest
 
 COMMUNITY_HUB_GRAPHQL_ENDPOINT = "https://robertsspaceindustries.com/community-hub/api/v1/graphql"
@@ -14,7 +12,7 @@ class CommunityHubClient:
     async def fetch_user_profile(self, profile: str):
         request = GraphQLRequest(
             query='query getAccount($CreatorQuery: CreatorQuery!) {\n  creator(query: $CreatorQuery) {\n    ...Account\n    __typename\n  }\n}\n\nfragment Account on Account {\n  bio\n  citizenDossierUrl\n  displayName\n  live\n  nickname\n  stats {\n    ...AccountStats\n    __typename\n  }\n  thumbnailUrl\n  website\n  __typename\n}\n\nfragment AccountStats on AccountStats {\n  followed\n  followedCount\n  followingCount\n  upvotesCount\n  viewsCount\n  __typename\n}',
-            variables={"CreatorQuery": {"nickname": "Khuzdul"}},
+            variables={"CreatorQuery": {"nickname": profile}},
             operation="getAccount",
             validate=False
         )
