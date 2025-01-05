a2a02e4d2a44eed6f8f3db9891277d64dc6d6870
henry232323 <14969260+henry232323@users.noreply.github.com>
2025-01-04 23:16:32 -0800

    Factor querystring out of GraphQL request

--- src/spectrum/community/client.py
+++ src/spectrum/community/client.py
@@ -9,12 +9,16 @@ class CommunityHubClient:
             endpoint=COMMUNITY_HUB_GRAPHQL_ENDPOINT,
         )
 
-    async def fetch_user_profile(self, profile: str):
-        request = GraphQLRequest(
+    @staticmethod
+    def get_user_profile_querystring(profile: str):
+        return GraphQLRequest(
             query='query getAccount($CreatorQuery: CreatorQuery!) {\n  creator(query: $CreatorQuery) {\n    ...Account\n    __typename\n  }\n}\n\nfragment Account on Account {\n  bio\n  citizenDossierUrl\n  displayName\n  live\n  nickname\n  stats {\n    ...AccountStats\n    __typename\n  }\n  thumbnailUrl\n  website\n  __typename\n}\n\nfragment AccountStats on AccountStats {\n  followed\n  followedCount\n  followingCount\n  upvotesCount\n  viewsCount\n  __typename\n}',
             variables={"CreatorQuery": {"nickname": profile}},
             operation="getAccount",
             validate=False
         )
+
+    async def fetch_user_profile(self, profile: str):
+        request = self.get_user_profile_querystring(profile)
         resp = await self._client.query(request=request)
         return resp.data
